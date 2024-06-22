aca3555e6a65fe89f717228b6064ca2891ef8fe0
Grygoriy Zolotarov <zolotaryovgl@gmail.com>
2024-06-22 22:29:27 +0200

    added utrs

--- commands/coverage.py
+++ commands/coverage.py
@@ -72,9 +72,15 @@ def check_temp(temp, logger):
     else:
         logger.info(f'Temporary directory found: {temp}')
 
-def filter_and_merge(bedgraph, outfile, logger, cov_threshold=2):
-    filter_and_merge_command = f"awk '$4>={cov_threshold}' {bedgraph}  | bedtools merge -i - -c 4,5,6 -o distinct,distinct,distinct > {outfile}"
+def filter_and_merge(bedgraph, outfile, cov_threshold,strand,logger):
+    if strand == 'unstranded':
+        merge_sub = '-c 4 -o sum'
+    elif strand == 'stranded':
+        merge_sub = '-c 4,5,6 -o distinct,sum,distinct'
+
+    filter_and_merge_command = f"awk '$4>={cov_threshold}' {bedgraph}  | bedtools merge -i - {merge_sub} > {outfile}"
     run_command(filter_and_merge_command, logger)
+
     if is_file_empty(outfile):
         logger.info(f'ERROR: {outfile} is empty!')
         quit()
@@ -92,6 +98,7 @@ def coverage_main(args):
     logger.info(" --".join([f"{key} : {value}" for key, value in args.__dict__.items()]))
 
     try:
+        coverage_thr = 2
         check_temp(temp, logger)
         # Index BAM file
         index_bam(bam_file, logger)
@@ -102,32 +109,32 @@ def coverage_main(args):
         
         if strand == 'unstranded':
             bam_coverage(bam_file, bedgraph_file, cpu_count, None, logger)
-            filter_and_merge(bedgraph_file, filtered_bed, logger)
+            filter_and_merge(bedgraph_file, filtered_bed, coverage_thr, strand, logger)
         else:
             for_bedgraph_file = os.path.join(temp, 'for.bedgraph')
             for_filtered_bed = os.path.join(temp,'for.reg.bed')
             rev_bedgraph_file = os.path.join(temp, 'rev.bedgraph')
             rev_filtered_bed = os.path.join(temp,'rev.reg.bed')
 
-        if not os.path.exists(for_bedgraph_file): 
-            bam_coverage(bam_file, for_bedgraph_file, cpu_count, 'forward', logger)
-        else:
-            logger.info(f'{for_bedgraph_file} found')
-        if not os.path.exists(for_filtered_bed): 
-            filter_and_merge(for_bedgraph_file,for_filtered_bed,logger)
-        else: logger.info(f'{for_filtered_bed} exists')
-        if not os.path.exists(rev_bedgraph_file): 
-            bam_coverage(bam_file, rev_bedgraph_file, cpu_count, 'reverse', logger)
-        else:
-            logger.info(f'{rev_bedgraph_file} exists.')
-        if not os.path.exists(rev_filtered_bed): 
-            filter_and_merge(rev_bedgraph_file,rev_filtered_bed,logger)
-        else:
-            logger.info(f'{rev_filtered_bed} exists.')
-        if not os.path.exists(filtered_bed):
-            merge_strands(for_bed = for_filtered_bed,rev_bed = rev_filtered_bed,outfile = filtered_bed)
-        else:
-            logger.info(f'{filtered_bed} exists.')
+            if not os.path.exists(for_bedgraph_file): 
+                bam_coverage(bam_file, for_bedgraph_file, cpu_count, 'forward', logger)
+            else:
+                logger.info(f'{for_bedgraph_file} found')
+            if not os.path.exists(for_filtered_bed): 
+                filter_and_merge(for_bedgraph_file,for_filtered_bed,coverage_thr, strand, logger)
+            else: logger.info(f'{for_filtered_bed} exists')
+            if not os.path.exists(rev_bedgraph_file): 
+                bam_coverage(bam_file, rev_bedgraph_file, cpu_count, 'reverse', logger)
+            else:
+                logger.info(f'{rev_bedgraph_file} exists.')
+            if not os.path.exists(rev_filtered_bed): 
+                filter_and_merge(rev_bedgraph_file,rev_filtered_bed,coverage_thr, strand, logger)
+            else:
+                logger.info(f'{rev_filtered_bed} exists.')
+            if not os.path.exists(filtered_bed):
+                merge_strands(for_bed = for_filtered_bed,rev_bed = rev_filtered_bed,outfile = filtered_bed)
+            else:
+                logger.info(f'{filtered_bed} exists.')
     except Exception as e:
         logger.error(f"An error occurred during coverage processing: {e}")
 
---
+++ commands/utrs.py
@@ -0,0 +1,64 @@
+import subprocess
+import os 
+import logging
+logger = logging.getLogger('3utr.utrs')
+
+# Should allow calling the 3'-utrs from the coverage bed
+
+
+def run_command(command, logger):
+    logger.info(f"Executing: {command}")
+    try:
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            logger.error(f"Command failed with error:\n{stderr.decode()}")
+            raise Exception(f"Command failed with error:\n{stderr.decode()}\nCheck log.")
+        return stdout.decode()
+    except Exception as e:
+        logger.error(f"Failed to execute command: {command}. Error: {e}")
+        raise
+
+def slice_regs(coverage_regions,stop_bed,output,strand,logger):
+    if strand == "unstranded":
+        command = f"""bedtools intersect -a {coverage_regions} -b {stop_bed} -wa -wb | \
+awk 'BEGIN{{OFS="\\t"}}{{if($10=="+"){{print $1,$6,$3,$8,0,$10}}else{{print $1,$2,$7,$8,0,$10}}}}' | \
+awk '$3>$2' > {output}"""
+    elif strand == "stranded":
+        command = f"""bedtools intersect -a {coverage_regions} -b {stop_bed} -wa -wb -s | \
+awk 'BEGIN{{OFS="\\t"}}{{if($6=="+"){{print $1,$8,$3,$4":"$10,0,$6}}else{{print $1,$2,$8,$4":"$10,0,$6}}}}' | \
+awk '$2>0&&$3>$2' > {output}"""
+    run_command(command,logger)
+
+def outersect_with_genes(bed,genes_bed,output,tmp_ids,strand,logger):
+    command = f"""
+    # create a list of UTRs overlapping more than one gene.
+    bedtools intersect -a {bed} -b {genes_bed} -wa -wb -s | \
+    awk '{{print $4"\\t"$10}}' | cut -f 1 | sort | uniq -c | \
+    awk '$1>1 {{print $2}}' > {tmp_ids}
+    awk 'BEGIN{{OFS="\\t"}} FNR==NR {{o[$1]=$1;next}}{{if(!($4 in o)){{print $0}}}}' {tmp_ids} {bed} > {output}
+    """
+    run_command(command,logger)
+
+
+def utrs_main(args):
+    input_cov = args.cov
+    input_stop = args.stop
+    genes_bed = args.genes 
+    strand = args.strand 
+    output_file = args.out
+    temp = args.temp
+    
+    init_3utrs = os.path.join(temp,'3utrs.bed')
+    tmp_ids = os.path.join(temp,'ids.txt')
+    slice_regs(input_cov,input_stop,init_3utrs,strand,logger)
+    outersect_with_genes(init_3utrs,genes_bed,output_file,tmp_ids,strand,logger)
+
+
+
+#GENES=Avaga.genes.bed
+#bedtools intersect -a 3utr.bed -b Avaga.genes.bed -s -v > 3utr.noov.bed
+# create a list of utrs overlapping more than one gene.
+#bedtools intersect -a 3utr.bed -b $GENES -wa -wb -s | awk '{print $4"\t"$10}' | cut -f 1 | sort | uniq -c  | awk '$1>1 {print $2}' > offending.txt
+#awk 'BEGIN{OFS="\t"} FNR==NR {o[$1]=$1;next}{if($4 in o == 0){print $0}}' offending.txt 3utr.bed   > 3utr.noov.bed
+
